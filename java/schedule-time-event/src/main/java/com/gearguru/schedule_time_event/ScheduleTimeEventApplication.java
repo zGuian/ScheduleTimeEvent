99e07918ab9d5c98354ea73bd70ed524a8de6eac
zGuian <guianmarcos32@gmail.com>
2024-11-06 10:13:26 -0300

    Adicionado projeto Java

---
+++ java/schedule-time-event/src/main/java/com/gearguru/schedule_time_event/ScheduleTimeEventApplication.java
@@ -0,0 +1,13 @@
+package com.gearguru.schedule_time_event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleTimeEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleTimeEventApplication.class, args);
+	}
+
+}
---
+++ java/schedule-time-event/src/test/java/com/gearguru/schedule_time_event/ScheduleTimeEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gearguru.schedule_time_event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleTimeEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
